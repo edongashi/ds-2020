65bbb45cf3170779da54e12c6b32f7bfba94533e
Edon Gashi <edon@aircloak.com>
2020-03-21 00:29:17 +0100

    Add DesStream

---
+++ DesStream/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DesStream
+{
+    class Program
+    {
+        private static readonly byte[] iv = Encoding.UTF8.GetBytes("12345678");
+
+        static void Main(string[] args)
+        {
+            // des encrypt <input-file> <out-file>
+            // des encrypt
+            // des decrypt <input-file> <out-file>
+            // des decrypt
+
+            var key = Encoding.UTF8.GetBytes("fiek2018");
+            var command = args.FirstOrDefault();
+
+            Stream OpenInput()
+            {
+                return args.Length >= 2
+                    ? File.OpenRead(args[1])
+                    : Console.OpenStandardInput();
+            }
+
+            Stream OpenOutput()
+            {
+                return args.Length >= 3
+                    ? File.OpenWrite(args[2])
+                    : Console.OpenStandardOutput();
+            }
+
+            switch (command)
+            {
+                case "encrypt":
+                    Encrypt(key, OpenInput(), OpenOutput());
+                    break;
+
+                case "decrypt":
+                    Decrypt(key, OpenInput(), OpenOutput());
+                    break;
+
+                default:
+                    Usage();
+                    break;
+            }
+        }
+
+        private static void Usage()
+        {
+            Console.WriteLine("Argumente jo valide.");
+        }
+
+        private static void Encrypt(byte[] key, Stream inputStream, Stream outputStream)
+        {
+            using (var des = new DESCryptoServiceProvider
+            {
+                Key = key,
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.Zeros,
+                IV = iv
+            })
+            using (var encryptor = des.CreateEncryptor())
+            using (var cryptoStream = new CryptoStream(
+                outputStream,
+                encryptor,
+                CryptoStreamMode.Write)
+            )
+            {
+                inputStream.CopyTo(cryptoStream);
+            }
+        }
+
+        private static void Decrypt(byte[] key, Stream inputStream, Stream outputStream)
+        {
+            using (var des = new DESCryptoServiceProvider
+            {
+                Key = key,
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.Zeros,
+                IV = iv
+            })
+            using (var decryptor = des.CreateDecryptor())
+            using (var cryptoStream = new CryptoStream(
+                inputStream,
+                decryptor,
+                CryptoStreamMode.Read)
+            )
+            {
+                cryptoStream.CopyTo(outputStream);
+            }
+        }
+    }
+}
---
+++ Hex/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Hex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arg = args.FirstOrDefault();
+            if (arg == "encode" || arg == null)
+            {
+                var inputBytes = ReadToEnd(Console.OpenStandardInput());
+                Console.WriteLine(ByteArrayToString(inputBytes));
+            }
+            else if (arg == "decode")
+            {
+                var hexString = Console.In.ReadToEnd();
+                var outputBytes = StringToByteArray(hexString);
+                var outputStream = Console.OpenStandardOutput();
+                outputStream.Write(outputBytes, 0, outputBytes.Length);
+                outputStream.Flush();
+                outputStream.Close();
+            }
+            else
+            {
+                Console.WriteLine("Invalid args.");
+            }
+
+        }
+        public static byte[] StringToByteArray(string hex)
+        {
+            hex = Regex.Replace(hex, @"\s", "");
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            return BitConverter.ToString(ba).Replace("-", "");
+        }
+
+        public static byte[] ReadToEnd(Stream stream)
+        {
+            long originalPosition = 0;
+
+            if (stream.CanSeek)
+            {
+                originalPosition = stream.Position;
+                stream.Position = 0;
+            }
+
+            try
+            {
+                byte[] readBuffer = new byte[4096];
+
+                int totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0)
+                {
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead == readBuffer.Length)
+                    {
+                        int nextByte = stream.ReadByte();
+                        if (nextByte != -1)
+                        {
+                            byte[] temp = new byte[readBuffer.Length * 2];
+                            Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+                            Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            readBuffer = temp;
+                            totalBytesRead++;
+                        }
+                    }
+                }
+
+                byte[] buffer = readBuffer;
+                if (readBuffer.Length != totalBytesRead)
+                {
+                    buffer = new byte[totalBytesRead];
+                    Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+                }
+
+                return buffer;
+            }
+            finally
+            {
+                if (stream.CanSeek)
+                {
+                    stream.Position = originalPosition;
+                }
+            }
+        }
+    }
+}
